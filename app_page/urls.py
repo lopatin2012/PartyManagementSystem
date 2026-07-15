63af369e3c042839bce0f99c6931151959dc8153
HaHo4eJIoBeK <lopatin.maxim2012@yandex.ru>
2026-07-15 13:00:40 +0300

    feat: Добавлена домашняя страница без функционала, только для вида.

--- app_page/urls.py
+++ app_page/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from app_page import views
 
 urlpatterns = [
-
+    path('', view=views.MainPageView.as_view(), name='home')
 ]
--- app_page/views.py
+++ app_page/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+# app_page/views.py
 
-# Create your views here.
+import logging
+
+from django.views.generic import TemplateView
+
+from config.settings import (
+    DEBUG, SERVICE_MODE_TEXT, SERVICE_MODE_COLOR, SERVICE_VERSION
+)
+
+logger = logging.getLogger(__name__)
+
+class MainPageView(TemplateView):
+    """Главная страница."""
+    template_name = 'main/main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Определяем имя пользователя.
+        if self.request.user.is_authenticated:
+            user_name = (
+                    self.request.user.get_full_name()
+                    or self.request.user.username
+            )
+        else:
+            user_name = 'Неизвестный'
+
+        # Имитация счётчика.
+        requests_count = 1000
+
+        context.update(
+            {
+                'title_name': 'Система управления партиями',
+                'page_name': 'Главная страница',
+                'user_name': user_name,
+                'is_authenticated': self.request.user.is_authenticated,
+                'service_version': SERVICE_VERSION,
+                'requests_count': requests_count,
+                'service_mode_name': SERVICE_MODE_TEXT,
+                'service_mode_color': SERVICE_MODE_COLOR
+            }
+        )
+
+        return context
