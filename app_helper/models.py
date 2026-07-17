0105e72e2546b4f4d56b644f9742e644532b7cd8
HaHo4eJIoBeK <lopatin.maxim2012@yandex.ru>
2026-07-17 15:42:33 +0300

    fix: Исправлена обработка uuid7

--- app_helper/models.py
+++ app_helper/models.py
@@ -1,10 +1,31 @@
+import uuid
+
 from uuid_utils import uuid7
 
 from django.db import models
 
+class UUID7Field(models.UUIDField):
+    """Кастомный UUIDv7"""
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('default', uuid7)
+        kwargs.setdefault('editable', False)
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if value is None:
+            return None
+
+        if isinstance(value, uuid.UUID):
+            return value
+
+        try:
+            return uuid.UUID(str(value))
+        except (ValueError, TypeError, AttributeError):
+            raise ValueError('Значение не является корректным UUID.')
+
 class UUIDModel(models.Model):
     """Абстрактная модель с UUID в качестве первичного ключа."""
-    id = models.UUIDField(
+    id = UUID7Field(
         primary_key=True,
         default=uuid7,
         editable=False,
